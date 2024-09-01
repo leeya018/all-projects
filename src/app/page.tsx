b4bb8ff5d5ba437d7657ca4831097a5bf0b86782
leeya018 <leeyahav018@gmail.com>
2024-09-01 03:57:35 +0300

    tsr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,26 +15,31 @@ export default function MainPage() {
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
         <ProjectCard projectInd={projectInd} />
       </Modal>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <h1 className="mt-10 text-3xl font-semibold flex justify-center ">
+        My Projects
+      </h1>
+      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projectDetails.map((project, key) => (
-          <div
-            key={key}
-            className="rounded-t-lg  pb-6  border  border-gray-700  overflow-hidden flex flex-col justify-between"
-          >
+          <div className="rounded-t-lg  pb-6  border  border-gray-700  overflow-hidden flex flex-col justify-between">
             <Link key={key} href={project.link} target="_blank">
-              <div className="border-2 flex justify-center items-center">
-                <Image
-                  width={300}
-                  height={500}
-                  src={project.folderUrl + "/1.png"}
-                  alt={`Project Image ${key}`}
-                  className="object-cover cursor-pointer  w-full h-60 hover:scale-105 duration-500"
-                />
+              <div key={key}>
+                <div className="border-2 flex justify-center items-center">
+                  <Image
+                    width={300}
+                    height={500}
+                    src={project.folderUrl + "/1.png"}
+                    alt={`Project Image ${key}`}
+                    className="object-cover cursor-pointer  w-full h-60 hover:scale-105 duration-500"
+                  />
+                </div>
+
+                <div className="p-3">
+                  <h4 className="text-lg font-semibold mt-4">{project.name}</h4>
+                  <p className="mt-4 line-clamp-3">{project.description}</p>
+                </div>
               </div>
             </Link>
-            <div className="p-3">
-              <h4 className="text-lg font-semibold mt-4">{project.name}</h4>
-              <p className="mt-4 line-clamp-3">{project.description}</p>
+            <div className="p-3 cursor-pointer">
               <p
                 className="mt-4 line-clamp-3 underline text-blue-500 hover:text-blue-600"
                 onClick={(e) => {
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,15 +10,15 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ projectInd }) => {
   if (projectInd === -1) return;
 
   return (
-    <div className="p-6 duration-300 relative">
+    <div
+      className="p-6 duration-300 relative overflow-y-scroll 
+      scrollbar-hide  max-h-[80vh]"
+    >
       <h2 className="text-2xl font-semibold">
         {projectDetails[projectInd].name}
       </h2>
       <p className="mt-2">{projectDetails[projectInd].description}</p>
-      <div
-        className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4 overflow-y-scroll 
-      scrollbar-hide  max-h-[60vh]"
-      >
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4 ">
         {Array(projectDetails[projectInd].imagesAmount)
           .fill(0)
           .map((_, index) => (
