3b09f576eab37a996117fff943e98c4225abe5d9
leeya018 <leeyahav018@gmail.com>
2024-09-01 05:48:43 +0300

    h

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -10,8 +10,8 @@ export default function ToggleButton({ type, onToggle }: ToggleButtonProps) {
   return (
     <button
       onClick={onToggle}
-      className={`px-4 py-2 rounded ${
-        type ? "bg-green-500" : "bg-red-500"
+      className={`px-4 py-2 w-20 rounded ${
+        type === project_types.my ? "bg-blue-500" : "bg-green-500"
       } text-white transition-colors duration-300`}
     >
       {type === project_types.my ? project_types.clients : project_types.my}
